f4c5118a7321fb46634db88081370310055af59e
Vikrant <vikrant.chopra@lexmark.com>
2017-01-03 10:34:36 +0530

    Added Home Page content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RoutingModule } from './app-routing.module';
 
 import { ButtonModule } from 'primeng/primeng';
+import {PanelModule} from 'primeng/primeng';
+
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    ButtonModule
+    ButtonModule,
+    PanelModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
